717c5282f4fb3c6f943bd6719817014ac3460c01
student <tomassiman7@gmail.com>
2018-08-08 15:07:21 +0200

    medzera

--- src/sk/tsystems/gamestudio/game/guessnumber/Guessnumber.java
+++ src/sk/tsystems/gamestudio/game/guessnumber/Guessnumber.java
@@ -3,8 +3,10 @@ package sk.tsystems.gamestudio.game.guessnumber;
 import java.util.Scanner;
 import java.util.Random;
 
-public class Guessnumber {
+public class Guessnumber  {
 
+	
+	
 	public static void main(String[] args) {
 		ConsoleUI consoleUI = new ConsoleUI();
 		consoleUI.play();
